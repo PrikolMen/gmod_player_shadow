5d7641061426db4b1460d8d3e759dcfb0da78800
PrikolMen:-b <olegis019@gmail.com>
2022-06-29 14:28:14 +0300

    better

--- lua/autorun/client/cl_player_shadow.lua
+++ lua/autorun/client/cl_player_shadow.lua
@@ -1,7 +1,7 @@
-local RunConsoleCommand = RunConsoleCommand
+local cvar = "cl_drawownshadow"
 local convar = "cl_playershadow"
 
-RunConsoleCommand( "cl_drawownshadow", CreateClientConVar( convar, "1", true, true, "Turn on/off shadow on firstpersion.", 0, 1 ):GetInt() )
+RunConsoleCommand( cvar, CreateClientConVar( convar, "1", true, true, "Turn on/off shadow on firstpersion.", 0, 1 ):GetInt() )
 cvars.AddChangeCallback( convar, function( name, old, new )
-    RunConsoleCommand( "cl_drawownshadow", new )
-end, "Player Shadow")
+    RunConsoleCommand( cvar, new )
+end, cvar)
